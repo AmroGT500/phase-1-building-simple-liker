78bdd0cac8bbbbc30d390002797737d8c5cedab1
Ammar Hussain <ammar92.h@gmail.com>
2023-06-01 05:18:01 -0400

    completed

--- main.js
+++ main.js
@@ -1,11 +1,54 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
-// Your JavaScript code goes here!
+// Get references to the heart elements
+const hearts = document.querySelectorAll('.like-glyph');
 
+// Iterate over the heart elements
+hearts.forEach((heart) => {
+  // Add click event listener to each heart
+  heart.addEventListener('click', () => {
+    // Check if the heart is currently empty or full
+    const isFull = heart.classList.contains('activated-heart');
 
+    // Reset the error modal
+    const errorModal = document.getElementById('modal');
+    errorModal.classList.add('hidden');
 
+    // Make the server request
+    mimicServerCall()
+      .then(() => {
+        if (isFull) {
+          // Change the heart back to empty
+          heart.innerText = EMPTY_HEART;
+          heart.classList.remove('activated-heart');
+        } else {
+          // Change the heart to full
+          heart.innerText = FULL_HEART;
+          heart.classList.add('activated-heart');
+        }
+      })
+      .catch((error) => {
+        // Display the error modal
+        const modalMessage = document.getElementById('modal-message');
+        modalMessage.innerText = error;
+        errorModal.classList.remove('hidden');
+
+        // Hide the error modal after 3 seconds
+        setTimeout(() => {
+          errorModal.classList.add('hidden');
+        }, 3000);
+      });
+  });
+});
+
+// the code selects all the heart elements with the class like-glyph using document.querySelectorAll('.like-glyph'). 
+// It then adds a click event listener to each heart element using addEventListener. 
+// Inside the event listener, the server request is made using mimicServerCall(). 
+// If the server request is successful (then block), the heart is changed to empty or full based on its current state. 
+// If the server request fails (catch block), the error modal is displayed, 
+// and after 3 seconds, it is hidden again using setTimeout.
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
